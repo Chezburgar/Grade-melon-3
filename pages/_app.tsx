39d6556e8e1ba9add4250503b9c774795611a842
Jonathan Shapiro <50190184+Jshap06@users.noreply.github.com>
2025-01-05 01:01:44 -0500

    Made verbose error actually work whoops

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -142,11 +142,12 @@ function MyApp({ Component, pageProps }) {
 	}, [client,districtURL]);
 
 	function createError(message:string){
+		console.log("Verbose Error: ",message)
 		const preSets={"upgraded":"API Token Expired, come back soon?","incorrect":"Username or Password is Incorrect","invalid":"Username or Password is Incorrect","load failed":"Network Error","failed to fetch":"Network Error:Try Again Later","socket":"Network Error"};
 		for(let key in preSets){
 			if(message.toLowerCase().includes(key)){var message=preSets[key];break}
 		}
-		console.log("Verbose Error: ",message)
+	
 		setToasts((toasts) => [...toasts, { title: message, type: "error" }]);
 			setTimeout(() => {
 				setToasts((toasts) => toasts.slice(1));
