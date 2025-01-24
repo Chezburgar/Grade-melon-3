e4a5145a52303aa02ee8bb20cabc6d62719a31fd
Jonathan <jonathanshapiro789@gmail.com>
2025-01-24 16:51:41 -0500

    z-index of toasts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -223,7 +223,7 @@ const logout = async () => {
             }}
           />
 			</Head>
-			<div className="absolute p-5 z-20">
+			<div className="fixed p-5 z-[60]">
 				{toasts.map(({ title, type }, i) => (
 					<div className="mb-5 z-50" key={i}>
 						<Toast>
