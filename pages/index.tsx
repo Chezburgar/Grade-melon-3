58d119d7b6222636412272b0c9b6055ac560d68f
Jonathan Shapiro <50190184+Jshap06@users.noreply.github.com>
2025-03-16 19:04:08 -0400

    Seo index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home({ client }: HomeProps) {
 						transition={{ delay: 0.2, duration: 0.5 }}
 						className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
 					>
-						Tired of using StudentVue? Grade Melon is an all new third party app
+						Tired of using StudentVue? Grade Melon is an all new third party student vue alternative
 						to help you stay in control of your grades. It allows any student
 						using Synergy StudentVue to login to check their schedule and
 						calculate their grades.
