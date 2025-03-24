e50069c7e421160d7480cbcc985e4323cba61705
Jonathan <jonathanshapiro789@gmail.com>
2025-03-24 16:16:53 -0400

    update lg breakpoint

--- pages/grades/[index].tsx
+++ pages/grades/[index].tsx
@@ -589,7 +589,7 @@ export default function Grades({
 							</tbody>
 						</table>
 					</div>
-					{<div className="hidden xl:block shrink"><CustomAd timestamp={timestamp} setTime={setTime} ad={ad} setAd={setAd}/> </div>
+					{<div className="hidden lg:block shrink"><CustomAd timestamp={timestamp} setTime={setTime} ad={ad} setAd={setAd}/> </div>
 					}
 					</div>
 				</motion.div>
