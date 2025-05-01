4714e5e9b954dc1b1a6581f1b08fcc220055911b
Jonathan <jonathanshapiro789@gmail.com>
2025-05-01 08:19:09 -0400

    modified gradeField for desktop breakpoint

--- components/GradeField.tsx
+++ components/GradeField.tsx
@@ -35,7 +35,7 @@ export default function GradeField({ value, onChange }: GradeFieldProps) {
 					type="number"
 					value={valasString}
 					onChange={onUpdate}
-					className="w-12 inline-block bg-gray-50 border-none bg-transparent p-0 text-gray-900 sm:text-xs rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+					className="w-12 inline-block bg-gray-50 border-none bg-transparent p-2 md:p-1 text-gray-900 sm:text-xs rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 				/>
 			)}
 		</div>
