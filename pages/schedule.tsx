c58e27d704278ad358c8c54327359c7042402777
Jonathan <jonathanshapiro789@gmail.com>
2025-05-12 12:23:36 -0400

    silly state preservaton stuff

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -33,12 +33,12 @@ export default function Schedule({ client,createError }: ScheduleProps) {
 				console.log("schedule is here",res);
 				setSchedule(res);
 				if(res.today===false){setToday(false);setTerm(res.termIndex)}
-				else{
-					setToday(true);
-				}
+
 				setLoading(false);
 			
-			}).catch(err=>{createError(err.message);console.log(err,term,today)});}else{console.log(today,term);setSchedule(client.loadedSchedule);setLoading(false)}
+			}).catch(err=>{createError(err.message);console.log(err,term,today)});}else{console.log(today,term);setSchedule(client.loadedSchedule);
+				if(client.loadedSchedule.today===false){setToday(false);setTerm(client.loadedSchedule.termIndex)}else{setToday(true)}
+				setLoading(false)}
 		} catch {
 			if (localStorage.getItem("remember") === "false") {
 				console.log("womp womp")
