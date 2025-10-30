dd9997b4a2657a03c6c1670607fc4e584e66c956
Jonathan Shapiro <jonathanshapiro789@gmail.com>
2025-10-30 19:59:07 -0400

    turn info cache back on for now. expensive.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -306,7 +306,7 @@ useEffect(()=>{
 
 	useEffect(()=>{ //Hook responsible for fetching studentInfo
 		if(client!==undefined&&studentInfo==undefined){
-			if(localStorage.getItem("infoCache")!=undefined&&false){ //temporarily disabling infoCache
+			if(localStorage.getItem("infoCache")!=undefined){ //temporarily re-enabling infoCache
 				const cache=JSON.parse(localStorage.getItem("infoCache"));
 				if(cache.user==client.username){
 					setStudentInfo(cache.info);
