bf4e7574e51f79b58cdf2e363156adc89f59a232
grade-berry <barbels.27.door@icloud.com>
2026-01-21 16:27:52 -0500

    Update next.config.js for for gh pages
    Added PWA disable option for development environment.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 const withPwa = require("next-pwa")({
-	dest: "public",
-	register: true,
-	skipWaiting: true,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPwa({
-	swcMinify: false,
-	reactStrictMode: true,
-	images: {
-		unoptimized: true,
-	},
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: false,
+
+  images: {
+    unoptimized: true, 
+  },
 });
 
 module.exports = nextConfig;
